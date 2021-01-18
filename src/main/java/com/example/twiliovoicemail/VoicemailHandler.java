3429cc8e683155ee969b8a074579c96b9f955c0a
Matthew Gilliard <matthew@gilliard.lol>
2021-01-18 15:14:48 +0000

    Adds implementation of /handle-unanswered-call endpoint

--- src/main/java/com/example/twiliovoicemail/VoicemailHandler.java
+++ src/main/java/com/example/twiliovoicemail/VoicemailHandler.java
@@ -1,14 +1,26 @@
 package com.example.twiliovoicemail;
 
+import com.twilio.Twilio;
 import com.twilio.http.HttpMethod;
 import com.twilio.twiml.VoiceResponse;
 import com.twilio.twiml.voice.Dial;
+import com.twilio.twiml.voice.Pause;
+import com.twilio.twiml.voice.Play;
+import com.twilio.twiml.voice.Record;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("TwiML")
 public class VoicemailHandler {
-    
+
+    static {
+        Twilio.init(
+            System.getenv("TWILIO_ACCOUNT_SID"),
+            System.getenv("TWILIO_AUTH_TOKEN"));
+    }
+
+
     private final String MY_CELLPHONE_NUMBER = System.getenv("MY_CELLPHONE_NUMBER");
     private final int ANSWERPHONE_TIMEOUT_SECONDS = 10;
 
@@ -20,6 +32,27 @@ public class VoicemailHandler {
                 .timeout(ANSWERPHONE_TIMEOUT_SECONDS)
                 .action("/handle-unanswered-call")
                 .method(HttpMethod.GET)
+                .build())
+            .build().toXml();
+    }
+
+    @GetMapping(value = "/handle-unanswered-call", produces = "application/xml")
+    public String handleUnansweredCall(@RequestParam("DialCallStatus") String dialCallStatus){
+
+        if ("busy".equals(dialCallStatus) || "no-answer".equals(dialCallStatus)){
+            return voicemailTwiml();
+        }
+
+        return null;
+    }
+
+    private String voicemailTwiml() {
+        return new VoiceResponse.Builder()
+            .pause(new Pause.Builder().length(2).build())
+            .play(new Play.Builder("/message.mp3").build())
+            .record(new Record.Builder()
+                .playBeep(true)
+                .action("/recordings")
                 .build())
             .build().toXml();
     }
