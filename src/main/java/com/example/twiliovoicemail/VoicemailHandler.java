3d0295d6f51228614c90927095874a1056b3c662
Matthew Gilliard <matthew@gilliard.lol>
2021-01-18 14:08:05 +0000

    Adds implementation of /initial-answer endpoint

--- src/main/java/com/example/twiliovoicemail/VoicemailHandler.java
+++ src/main/java/com/example/twiliovoicemail/VoicemailHandler.java
@@ -1,8 +1,27 @@
 package com.example.twiliovoicemail;
 
+import com.twilio.http.HttpMethod;
+import com.twilio.twiml.VoiceResponse;
+import com.twilio.twiml.voice.Dial;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("TwiML")
 public class VoicemailHandler {
+    
+    private final String MY_CELLPHONE_NUMBER = System.getenv("MY_CELLPHONE_NUMBER");
+    private final int ANSWERPHONE_TIMEOUT_SECONDS = 10;
+
+    @GetMapping(value = "/initial-answer", produces = "application/xml")
+    public String initialAnswer(){
+        return new VoiceResponse.Builder()
+            .dial(new Dial.Builder()
+                .number(MY_CELLPHONE_NUMBER)
+                .timeout(ANSWERPHONE_TIMEOUT_SECONDS)
+                .action("/handle-unanswered-call")
+                .method(HttpMethod.GET)
+                .build())
+            .build().toXml();
+    }
 
 }
